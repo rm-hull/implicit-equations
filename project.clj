9c6f38f6694cef9c755df2df6d24126f9e07cf17
Richard Hull <rm_hull@yahoo.co.uk>
2016-04-26 21:18:53 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
     :url "http://opensource.org/licenses/MIT"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
-    [rm-hull/infix "0.2.2"]]
+    [rm-hull/infix "0.2.6"]]
   :scm {:url "git@github.com:rm-hull/implicit-equations.git"}
   :plugins [
     [codox "0.9.1"] ]
