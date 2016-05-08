468be495caa8b74bdd9234ade252304af7d26de0
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-08 20:55:53 +0100

    Add some basic tests

---
+++ test/implicit_equations/image_test.clj
@@ -0,0 +1,31 @@
+(ns implicit-equations.image-test
+  (:import
+    [java.awt Graphics2D RenderingHints]
+    [java.awt.image BufferedImage])
+  (:require
+    [clojure.test :refer :all]
+    [implicit-equations.image :refer :all]))
+
+(deftest check-opacity
+  (is (= -0x00000001 (opacity 0xFFFFFF 0xFF)))
+  (is (= -0x01000000 (opacity 0x000000 0xFF)))
+  (is (= -0x00010000 (opacity 0xFF0000 0xFF))))
+
+(deftest check-alpha
+  (is (= 0xFF (alpha -0x00000001)))
+  (is (= 0xFF (alpha -0x01000000)))
+  (is (= 0xFF (alpha -0x00010000))))
+
+(deftest check-create-image
+  (let [img (create-image 34 97)]
+    (is (= 34 (.getWidth img)))
+    (is (= 97 (.getHeight img)))
+    (is (= BufferedImage/TYPE_INT_ARGB (.getType img)))))
+
+(deftest check-create-graphics
+  (let [img (create-image 91 388)
+        g2d (create-graphics img)]
+    (is (= RenderingHints/VALUE_STROKE_NORMALIZE (.getRenderingHint g2d RenderingHints/KEY_STROKE_CONTROL)))
+    (is (= RenderingHints/VALUE_ANTIALIAS_ON (.getRenderingHint g2d RenderingHints/KEY_ANTIALIASING)))
+    (is (= RenderingHints/VALUE_RENDER_QUALITY (.getRenderingHint g2d RenderingHints/KEY_RENDERING)))))
+
