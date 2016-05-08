2d20b65ee3f3f438dc05364b51a34a113525eb31
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-08 21:26:00 +0100

    Formatting

--- test/implicit_equations/plot_test.clj
+++ test/implicit_equations/plot_test.clj
@@ -8,6 +8,5 @@
   (is (= [10 7 -10 -7] (expand-bounds [10 7])))
   (is (= [10 7 5 -7] (expand-bounds [10 7 5])))
   (is (= [10 7 2 6] (expand-bounds [10 7 2 6])))
-  (is (= [0 1 2 3] (expand-bounds (range 100))))
-  )
+  (is (= [0 1 2 3] (expand-bounds (range 100)))))
 
