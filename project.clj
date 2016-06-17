eb9c1ea3b3bff5cc6f9eb99f4a11f4d356414dac
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-17 20:32:40 +0100

    Set correct opts for codox

--- project.clj
+++ project.clj
@@ -10,14 +10,13 @@
     [rm-hull/infix "0.2.8"]]
   :scm {:url "git@github.com:rm-hull/implicit-equations.git"}
   :plugins [
-    [codox "0.9.5"] ]
+    [lein-codox "0.9.5"] ]
   :source-paths ["src"]
   :jar-exclusions [#"(?:^|/).git"]
   :codox {
-    :sources ["src"]
-    :output-dir "doc/api"
-    :src-dir-uri "http://github.com/rm-hull/implicit-equations/blob/master/"
-    :src-linenum-anchor-prefix "L" }
+    :source-paths ["src"]
+    :output-path "doc/api"
+    :source-uri "http://github.com/rm-hull/implicit-equations/blob/master/{filepath}#L{line}" }
   :min-lein-version "2.6.1"
   :profiles {
     :dev {
