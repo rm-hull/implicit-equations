c416f15baac1aeb76a5c335fc20d681afee8608e
Richard Hull <rm_hull@yahoo.co.uk>
2016-06-19 00:11:41 +0100

    Use fork/join to embarrassingly parallelize the rendering
    Results in ~50-70% improvement on i5 MBP.
    see https://en.wikipedia.org/wiki/Embarrassingly_parallel

--- src/implicit_equations/plot.clj
+++ src/implicit_equations/plot.clj
@@ -3,6 +3,7 @@
     [java.awt.image BufferedImage])
   (:require
     [implicit-equations.image :refer :all]
+    [implicit-equations.scheduler :refer [task join]]
     [infix.macros :refer [infix]]))
 
 (defn expand-bounds [bounds]
@@ -25,7 +26,7 @@
             y (+ start-y (* j delta-y))]
         (neg? (eqn x y))))))
 
-(defn render! [^BufferedImage img f {:keys [bounds line-width width height rgb step] :as opts}]
+(defn render! [^BufferedImage img f [start end] {:keys [bounds line-width width height rgb step] :as opts}]
   (let [bounds (expand-bounds bounds)
         f (wrap-translation f bounds [width height])
         solid-color (opacity rgb 0xFF)
@@ -63,7 +64,7 @@
                       (doseq [y (range (inc top) bottom)]
                         (set-pixel x y solid-color))))]
 
-    (doseq [y (range height)
+    (doseq [y (range start end)
             x (range width)
             :let [sign (f x y)
                   nsignx1 (f (+ x 0.5) y)
@@ -86,16 +87,20 @@
   :step 0.02 })
 
 (comment
+  (def n-cpu (.availableProcessors (Runtime/getRuntime)))
 
   (use 'implicit-equations.plot)
   (use 'infix.macros)
   (use 'infix.math)
 
-  (defn draw [f name & [opts]]
+  (defn draw [concurrency f name & [opts]]
     (let [opts (merge default-opts opts)
           img (create-image (:width opts) (:height opts))
-          g2d (create-graphics img)]
-      (render! img f opts)
+          g2d (create-graphics img)
+          w (/ (:height opts) concurrency)
+          bands (map #(vector (* % w) (* (inc %) w)) (range concurrency))
+          tasks (map #(task (render! img f % opts)) bands)]
+      (doall (map join tasks))
       (write-png img name)
       (.dispose g2d)))
 
@@ -126,15 +131,40 @@
   (defn spira [x y]
     (infix sin(x ** 2 + y ** 2) - sin(x ÷ y ** 2)))
 
-  (time (draw quadrifolium "doc/quadrifolium.png" { :bounds 1 :line-width 4}))
-  (time (draw knot-curve "doc/knot-curve.png" { :bounds 5 :line-width 2}))
-  (time (draw biology "doc/biology.png"))
-  (time (draw chain-mesh "doc/chain-mesh.png"))
-  (time (draw checkerboard "doc/checkerboard.png"))
-  (time (draw dizzy "doc/dizzy.png"))
-  (time (draw bands "doc/bands.png" { :bounds (* 2 Math/PI) }))
-  (time (draw glint "doc/glint.png"))
-  (time (draw spira "doc/spira.png"))
+  ; 320ms vs 917ms
+  (time (draw 1 quadrifolium "doc/quadrifolium.png" { :bounds 1 :line-width 4}))
+  (time (draw 4 quadrifolium "doc/quadrifolium.png" { :bounds 1 :line-width 4}))
 
+  ; 276ms vs 704ms
+  (time (draw 1 knot-curve "doc/knot-curve.png" { :bounds 5 :line-width 2}))
+  (time (draw 4 knot-curve "doc/knot-curve.png" { :bounds 5 :line-width 2}))
+
+  ; 963 vs 2297ms
+  (time (draw 1 biology "doc/biology.png"))
+  (time (draw 4 biology "doc/biology.png"))
+
+  ; 1709ms vs 3204ms
+  (time (draw 1 chain-mesh "doc/chain-mesh.png"))
+  (time (draw 4 chain-mesh "doc/chain-mesh.png"))
+
+  ; 813ms vs 1736ms
+  (time (draw 1 checkerboard "doc/checkerboard.png"))
+  (time (draw 4 checkerboard "doc/checkerboard.png"))
+
+  ; 1344ms vs 2821ms
+  (time (draw 1 dizzy "doc/dizzy.png"))
+  (time (draw 4 dizzy "doc/dizzy.png"))
+
+  ; 984ms vs 2303ms
+  (time (draw 1 bands "doc/bands.png" { :bounds (* 2 Math/PI) }))
+  (time (draw 4 bands "doc/bands.png" { :bounds (* 2 Math/PI) }))
+
+  ; 1478ms vs 2804ms
+  (time (draw 1 glint "doc/glint.png"))
+  (time (draw 4 glint "doc/glint.png"))
+
+  ; 1870ms vs 3579ms
+  (time (draw 1 spira "doc/spira.png"))
+  (time (draw 4 spira "doc/spira.png"))
 )
 
